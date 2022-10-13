763b38fdaedb4db81aaafa962c93cd5bcfe32582
Ruben-JR <rubenpina758@gmail.com>
2022-10-13 14:11:16 -0100

    remove indent

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -6,27 +6,27 @@ import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, seScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, seScrolled] = useState(false);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if(window.scrollY > 50){
-                seScrolled(true);
-            }else{
-                seScrolled(false);
-            }
-        }
+  useEffect(() => {
+      const onScroll = () => {
+          if(window.scrollY > 50){
+              seScrolled(true);
+          }else{
+              seScrolled(false);
+          }
+      }
 
-        window.addEventListener("scroll", onScroll);
-        return () => window.removeEventListener("scroll", onScroll)
-    }, [])
+      window.addEventListener("scroll", onScroll);
+      return () => window.removeEventListener("scroll", onScroll)
+  }, [])
 
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
-    }
+  const onUpdateActiveLink = (value) => {
+      setActiveLink(value);
+  }
 
-    return (
+  return (
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
